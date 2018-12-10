eafd22d8906cf79dce9ed30703df12055c2769ab
akaberto <43548192+akaberto@users.noreply.github.com>
2018-12-10 18:05:15 +0100

    a

---
+++ Assets/ControlMarcador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public interface IMarcadorEventHandler : IEventSystemHandler
+{
+    void ActualizarPuntos(string nombreCoche, int puntos);
+}
+
+public class ControlMarcador : MonoBehaviour, IMarcadorEventHandler
+{
+    public void ActualizarPuntos(string nombreCoche, int puntos)
+    {
+        GetComponent<Text>().text = nombreCoche +  ": " +puntos;   
+    }
+}
---
+++ Assets/car.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public interface ICarEventHandler : IEventSystemHandler
+{
+    void FinalizarPartida();
+}
+
+public class car : MonoBehaviour, ICarEventHandler
+{
+    float velocidad = 100f;
+    public KeyCode teclaAsignada = KeyCode.F1;
+    int cuenta = 0;
+    bool estaParado = false;
+    public GameObject otroCoche;
+    public GameObject miMarcador;
+
+    void Update()
+    {
+        if (!estaParado) {
+            if (cuenta >= 26)
+            {
+                estaParado = true;
+                ExecuteEvents.Execute<ICarEventHandler>(otroCoche, null, (x, y) => x.FinalizarPartida());
+            }
+            else
+            {
+                if (Input.GetKeyDown(teclaAsignada))
+                {
+                    this.transform.Translate(new Vector3(0, velocidad, 0));
+                    cuenta++;
+                    ExecuteEvents.Execute<IMarcadorEventHandler>(miMarcador, null, (x, y) => x.ActualizarPuntos(this.name, cuenta));
+                }
+ 
+            }
+        }
+
+    }
+
+    public void FinalizarPartida()
+    {
+        estaParado = true;
+    }
+}
